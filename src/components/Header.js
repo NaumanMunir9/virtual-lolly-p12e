f429bef3534754cda7b16f82e36ca185e98f5806
NaumanMunir9 <naumanmunir9@gmail.com>
2021-09-11 09:58:00 +0500

    components

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header__component">
+      <h1>Virtual Lollipop</h1>
+      <p>because we all know someone who deserves some sugar</p>
+    </header>
+  );
+}
