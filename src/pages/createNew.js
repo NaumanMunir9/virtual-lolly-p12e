364791b18f8e5991702eee4d784fa294dddf67ba
NaumanMunir9 <naumanmunir9@gmail.com>
2021-09-11 11:47:50 +0500

    color picker

--- src/pages/createNew.js
+++ src/pages/createNew.js
@@ -1,9 +1,78 @@
+// libraries
 import React from "react";
+
+// components
+import Header from "../components/Header";
+import Lolly from "../components/Lolly";
 
 export default function CreateNewPage() {
   return (
     <div>
-      <h1>CreateNew</h1>
+      <Header />
+
+      <div>
+        <Lolly
+          fillLollyTop="#E97393"
+          fillLollyMiddle="#D03F5F"
+          fillLollyBottom="#BB3A5F"
+        />
+      </div>
+
+      <div className="flavours">
+        <label htmlFor="flavourTop" className="pickerLabel">
+          <input
+            type="color"
+            name="flavourTop"
+            id="flavourTop"
+            className="colorPicker"
+            value="#E97393"
+          />
+        </label>
+
+        <label htmlFor="flavourMiddle" className="pickerLabel">
+          <input
+            type="color"
+            name="flavourMiddle"
+            id="flavourMiddle"
+            className="colorPicker"
+            value="#D03F5F"
+          />
+        </label>
+
+        <label htmlFor="flavourBottom" className="pickerLabel">
+          <input
+            type="color"
+            name="flavourBottom"
+            id="flavourBottom"
+            className="colorPicker"
+            value="#BB3A5F"
+          />
+        </label>
+      </div>
+
+      <div className="form">
+        <div className="form__details">
+          <div>
+            <label htmlFor="recipientName">To</label>
+            <input
+              type="text"
+              name="recipientName"
+              id="recipientName"
+              placeholder="A lolly for..."
+            />
+          </div>
+
+          <div>
+            <label htmlFor="recipientName">To</label>
+            <input
+              type="text"
+              name="recipientName"
+              id="recipientName"
+              placeholder="A lolly for..."
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
