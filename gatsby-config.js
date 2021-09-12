505a422d2c06ab40b869d23e83e08b4c2e272cad
NaumanMunir9 <naumanmunir9@gmail.com>
2021-09-12 17:50:45 +0500

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,17 +11,5 @@ module.exports = {
         uri: "http://localhost:8888/.netlify/functions/newLolly",
       },
     },
-    `gatsby-plugin-material-ui`,
-    {
-      resolve: "gatsby-source-graphql",
-      options: {
-        // Arbitrary name for the remote schema Query type
-        typeName: "Lolly",
-        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
-        fieldName: "lolly",
-        // Url to query from
-        url: "https:localhost:8888/.netlify/functions/newLolly",
-      },
-    },
   ],
 };
